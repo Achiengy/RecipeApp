67eadd18b6b0ab3873e461d81974586592abcd84
+ <+>
2022-05-27 00:25:11 +0300

    Footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.scss'
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Meals Recipe App<span> by Achieng Pete</span> 2022 &copy;</p>
+        </div>
+    )
+}
+
+export default Footer
