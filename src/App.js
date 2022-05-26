9add26be31a5759ca311b790fa87b2e9d126d395
+ <+>
2022-05-27 00:17:43 +0300

    Adding navbar components and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'; 
+import Contact from './components/ContactPage/Contact';
+import Random  from './components/Categories/Categories';
+import About from './components/AboutPage/About';
+import HomePage from './components/HomePage/HomePage';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/random" component={Random}/>
+        <Route exact path="/contact" component={Contact}/>
+    </Switch>
+      <Footer />
+      </Router>
     </div>
   );
 }
