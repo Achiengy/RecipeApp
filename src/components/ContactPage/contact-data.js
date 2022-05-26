b7562c079647e5ff88d32bbecb19bdda098cf5b8
+ <+>
2022-05-27 00:39:28 +0300

    contact page data

---
+++ src/components/ContactPage/contact-data.js
@@ -0,0 +1,13 @@
+const contactConfig = {
+  Email: 'hope.pete@student.moringaschool.com',
+  Telephone: '(254)123-4567',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+};
+
+ 
+export {
+  
+  contactConfig, 
+  
+};
