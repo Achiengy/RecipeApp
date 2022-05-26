163c088df1db6f8c69a96fa77cb61c8f7128b17d
+ <+>
2022-05-27 01:31:51 +0300

    Edit the about content margin

---
+++ src/components/About/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+    return (
+      <div className="about">
+      <h2 class="title">About Us</h2>
+      <div class="about-content">
+          <div class="column left">
+              <img src="https://media.gettyimages.com/vectors/overhead-angle-of-a-person-using-apps-online-for-cooking-vector-id641803276?s=2048x2048" alt="" />
+          </div>
+          <div class="column right">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque harum accusamus corporis veniam fuga asperiores, beatae impedit. Perferendis quos reiciendis velit ad corporis aliquam rem recusandae consequatur ipsa accusamus, officia doloremque cum dolor enim cumque quam iste distinctio. Necessitatibus dignissimos consequatur fuga natus aspernatur fugit aliquid corrupti voluptatem laboriosam cum!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, nihil temporibus? Odio repudiandae repellat veniam saepe voluptatibus reiciendis suscipit illo quasi, nesciunt culpa laboriosam ipsa corporis minus dignissimos voluptate quibusdam. Nesciunt blanditiis, distinctio delectus suscipit nobis sequi praesentium consectetur modi incidunt dicta est repudiandae vel quam rerum inventore culpa quo?</p>
+          </div>
+      </div>
+      </div>
+    )
+  }
+  
+  export default About;
